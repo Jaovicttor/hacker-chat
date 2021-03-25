d239123a83c1df832c4ebaba30d49fc02395c0ed
João Victor dos Santos Oliveira <jvdso.15@gmail.com>
2021-03-25 14:12:15 -0300

    Dia 2
    O servidor e o client já estão conversando entre si

--- client/index.js
+++ client/index.js
@@ -1,7 +1,20 @@
+/*
+   "node index.js  --username JV --room sala001 --hostUri localhost"
+
+*/
 import TerminalController from "./src/terminalController.js"; //extenção .js é obrigatoria
 import Events from 'events'; //classe nativa do NodeJS para gestão de eventos 
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
+
+
+const [nodePath, filePath, ...commands] = process.argv;
+
+const config = CliConfig.parseArguments(commands);
 
 const componentEmiiter = new Events(); //Instancia da classe events (nativo)
-
+const socketClient = new SocketClient(config)
+await socketClient.initialize();
+/*
 const controller = new TerminalController(); //Instanciamento da classe TerminalController (desenvolvido) 
-await controller.initializeTable(componentEmiiter); 
+await controller.initializeTable(componentEmiiter); */
